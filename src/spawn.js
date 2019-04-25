3154ba29ce4cf1fa7fd8d34c41c1e8b032e7046c
Kevin Isom <kevin@isom.nz>
2019-04-25 19:13:52 +1200

    commands

---
+++ src/spawn.js
@@ -0,0 +1,14 @@
+const { spawn } = require('child_process')
+module.exports = async command =>
+  new Promise((resolve, reject) => {
+    const cmd = spawn(command, {
+      cwd: process.cwd(),
+      shell: true,
+      stdio: 'inherit',
+    })
+
+    cmd.on('exit', code => {
+      if (code === 0) return resolve(code)
+      return reject(code)
+    })
+  })
